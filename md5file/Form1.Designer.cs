198b3d53dfaadb77d20d8fb2d00bb44eaa82568e
Massimiliano Bassanetti <max.bassanetti@live.it>
2022-02-18 16:52:32 +0100

    Aggiungere i file di progetto.

---
+++ md5file/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace md5file
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.incollaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(559, 148);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(551, 122);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Crea MD5 del file";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.ContextMenuStrip = this.contextMenuStrip1;
+            this.textBox2.Location = new System.Drawing.Point(97, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(446, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copiaToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(106, 26);
+            // 
+            // copiaToolStripMenuItem
+            // 
+            this.copiaToolStripMenuItem.Name = "copiaToolStripMenuItem";
+            this.copiaToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.copiaToolStripMenuItem.Text = "Copia";
+            this.copiaToolStripMenuItem.Click += new System.EventHandler(this.copiaToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MD5 Generato :";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(369, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Genera MD5";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(468, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Sfoglia";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(97, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(365, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome del file :";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(551, 122);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Controlla file con MD5";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(410, 71);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "&Controlla MD5";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.ContextMenuStrip = this.contextMenuStrip2;
+            this.textBox4.Location = new System.Drawing.Point(97, 45);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(446, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Codice MD5 :";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(468, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "&Sfoglia";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(97, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(365, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nome del file :";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.incollaToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(110, 26);
+            // 
+            // incollaToolStripMenuItem
+            // 
+            this.incollaToolStripMenuItem.Name = "incollaToolStripMenuItem";
+            this.incollaToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.incollaToolStripMenuItem.Text = "Incolla";
+            this.incollaToolStripMenuItem.Click += new System.EventHandler(this.incollaToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(559, 148);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bassanetti - MD5 Tools 1.2";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copiaToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem incollaToolStripMenuItem;
+    }
+}
+
---
+++ md5file/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace md5file
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InstallEventHandlers(this);
+        }
+        private Pen borderColor = Pens.Red;
+        public Pen ActiveControlAngleColor { get { return this.borderColor; } set { this.borderColor = value; this.Refresh(); } }
+        public void InstallEventHandlers(Control containerControl)
+        {
+            foreach (Control c in containerControl.Controls)
+            {
+                if (c is Button) c.Cursor = Cursors.Hand;
+            }
+            containerControl.Paint -= ContainerControl_Paint;
+            containerControl.Paint += ContainerControl_Paint;
+            foreach (Control nestedControl in containerControl.Controls)
+            {
+                nestedControl.Enter -= Control_ReceivedFocus;
+                nestedControl.Enter += Control_ReceivedFocus;
+                if (nestedControl is ScrollableControl ||
+                    nestedControl is TabControl ||
+                    nestedControl is TabPage ||
+                    nestedControl is Panel ||
+                    nestedControl is GroupBox ||
+                    nestedControl is DataGrid ||
+                    nestedControl is ToolStrip ||
+                    nestedControl is MenuStrip)
+                    InstallEventHandlers(nestedControl);
+            }
+        }
+        private void Control_ReceivedFocus(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+        private void ContainerControl_Paint(object sender, PaintEventArgs e)
+        {
+            Control activeControl = this.ActiveControl;
+            if (ActiveControl != null && activeControl.Parent == sender)
+            {
+                int vline = 8;
+                int hline = 8;
+                Rectangle rect;
+                if (activeControl is CheckBox)
+                {
+                    rect = new Rectangle(activeControl.Location.X - 2, activeControl.Location.Y - 2, activeControl.Size.Width + 2, activeControl.Size.Height + 2);
+                }
+                else
+                {
+                    rect = new Rectangle(activeControl.Location.X - 2, activeControl.Location.Y - 2, activeControl.Size.Width + 3, activeControl.Size.Height + 3);
+                }
+                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                if (vline * 2 > rect.Size.Height) vline = (rect.Size.Height / 2) - 2;
+                if (hline * 2 > rect.Size.Width) hline = (rect.Size.Width / 2) - 2;
+                // Draw Angle NO
+                e.Graphics.DrawLine(borderColor, rect.X, rect.Y, rect.X + hline, rect.Y);
+                e.Graphics.DrawLine(borderColor, rect.X, rect.Y, rect.X, rect.Y + vline);
+                // Draw Angle NE
+                e.Graphics.DrawLine(borderColor, rect.X + rect.Width - hline, rect.Y, rect.X + rect.Width, rect.Y);
+                e.Graphics.DrawLine(borderColor, rect.X + rect.Width, rect.Y, rect.X + rect.Width, rect.Y + vline);
+                // Draw Angle SE
+                e.Graphics.DrawLine(borderColor, rect.X + rect.Width, rect.Y + rect.Height - vline, rect.X + rect.Width, rect.Y + rect.Height);
+                e.Graphics.DrawLine(borderColor, rect.X + rect.Width - hline, rect.Y + rect.Height, rect.X + rect.Width, rect.Y + rect.Height);
+                // Draw Angle SO
+                e.Graphics.DrawLine(borderColor, rect.X, rect.Y + rect.Height - vline, rect.X, rect.Y + rect.Height);
+                e.Graphics.DrawLine(borderColor, rect.X, rect.Y + rect.Height, rect.X + hline, rect.Y + rect.Height);
+            }
+        }
+        protected string GetMD5HashFromFile(string fileName)
+        {
+            try
+            {
+                FileStream file = new FileStream(fileName, FileMode.Open);
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] retVal = md5.ComputeHash(file);
+                file.Close();
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < retVal.Length; i++)
+                {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Il file potrebbe essere in uso\nChiuderlo o riprovare più tardi", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return "";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog d = new OpenFileDialog();
+            d.Title = "Apri file per creare MD5";
+            d.FileName = "";
+            d.Filter = "Tutti i file (*.*)|*.*";
+            d.CheckFileExists = true;
+            d.CheckPathExists = true;
+            if(d.ShowDialog()==DialogResult.OK)
+            {
+                textBox1.Text = d.FileName;
+                button2.Enabled = true;
+            }
+            else
+            {
+                textBox1.Text = "";
+                button2.Enabled = false;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string file = textBox1.Text;
+            string md5 = GetMD5HashFromFile(file);
+            if (md5=="")
+            {
+                textBox1.Text = ""; button2.Enabled = false;
+            }else
+            {
+                textBox2.Text = md5;
+            }
+        }
+        private void copiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox2.Text);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            o.Title = "Apri file da controllare";
+            o.FileName = "";
+            o.Filter = "Tutti i file (*.*)|*.*";
+            o.CheckFileExists = true;
+            o.CheckPathExists = true;
+            if(o.ShowDialog()==DialogResult.OK)
+            {
+                textBox3.Text = o.FileName;
+            }
+            else { textBox3.Text = ""; }
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            button4.Enabled = (textBox4.Text.Length > 8);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string md5 = textBox4.Text;
+            string file = textBox3.Text;
+            string md5file = GetMD5HashFromFile(file);
+            if(string.IsNullOrEmpty(md5file)==true)
+            {
+                MessageBox.Show("E' stato rilevato un errore durante il calcolo md5 del file", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if(md5!=md5file)
+                {
+                    MessageBox.Show("Codice MD5 non uguale, file non sicuro", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Codice MD5 uguale.", "MD5 corretto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void incollaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(Clipboard.ContainsText()==true)
+            {
+                textBox4.Text = Clipboard.GetText(TextDataFormat.Text);
+            }
+        }
+    }
+}
